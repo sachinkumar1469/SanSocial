6954efe6f830556bd6a5749717f4ba38a5cd3ecf
Sachin Kumar <91049993+sachinkumar1469@users.noreply.github.com>
2022-11-14 22:57:59 +0530

    Add files via upload

---
+++ sansocial_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name:'comment',
+    title:"Comment",
+    type:'document',
+    fields:[
+        {
+            name:'postedBy',
+            title:'PostedBY',
+            type:'postedBy'
+        },
+        {
+            name:'comment',
+            title:'comment',
+            type:'string'
+        }
+    ]
+}
---
+++ sansocial_backend/schemas/postedBy.js
@@ -0,0 +1,6 @@
+export default {
+    name:'postedBy',
+    title:'PostedBY',
+    type:'reference',
+    to:[{type:'user'}]
+}
